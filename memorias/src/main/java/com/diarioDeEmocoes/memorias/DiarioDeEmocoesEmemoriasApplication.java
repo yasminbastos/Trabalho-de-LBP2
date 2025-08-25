14a83ec528aada5b431214660f56f53a4d1ec00d
MayMatilde <matildemayara7@gmail.com>
2025-08-25 19:52:11 -0300

    Jesus Vive!

---
+++ memorias/src/main/java/com/diarioDeEmocoes/memorias/DiarioDeEmocoesEmemoriasApplication.java
@@ -0,0 +1,13 @@
+package com.diarioDeEmocoes.memorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDeEmocoesEmemoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDeEmocoesEmemoriasApplication.class, args);
+	}
+
+}
---
+++ memorias/src/test/java/com/diarioDeEmocoes/memorias/DiarioDeEmocoesEmemoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarioDeEmocoes.memorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDeEmocoesEmemoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
