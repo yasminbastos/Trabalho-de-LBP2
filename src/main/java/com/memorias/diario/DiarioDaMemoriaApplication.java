bfb37bae0e349e8bea9f9ffe0e27cdd2b76d71d1
MayMatilde <matildemayara7@gmail.com>
2025-09-08 18:55:29 -0300

    Projeto Java

---
+++ src/main/java/com/memorias/diario/DiarioDaMemoriaApplication.java
@@ -0,0 +1,13 @@
+package com.memorias.diario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioDaMemoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioDaMemoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memorias/diario/DiarioDaMemoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorias.diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioDaMemoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
